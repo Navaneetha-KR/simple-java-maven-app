f96191df3a4a89376f15ada9ff0a40273cbd9502
Navaneetha K R <71487980+Navaneetha-KR@users.noreply.github.com>
2024-06-11 11:19:47 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,4 +23,4 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar target/${NAME}-${VERSION}.jar
+java -jar https://github.com/Navaneetha-KR/simple-java-maven-app/blob/master/${NAME}-${VERSION}.jar
